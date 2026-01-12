60fabe7bcb7d149540963a53d84e0a8a2f5afb7d
Jinny926 <qsdfg0926@gmail.com>
2026-01-12 11:33:15 +0900

    docs: MEMO

---
+++ src/main/java/com/meta/springprepare/memo/Memo.java
@@ -0,0 +1,12 @@
+package com.meta.springprepare.memo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Memo {
+    private String username;
+    private String contents;
+}
+
